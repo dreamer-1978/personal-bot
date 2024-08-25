46c4273456d6e65dbf97a5451b664b4cd0043462
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 07:32:18 +0300

    add bot

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ const PORT = 3000
 const TOKEN = process.env.BOT_TOKEN;
 
 // if (!TOKEN) throw new Error("Token not Found!");
-// const bot = new Bot(TOKEN);
+const bot = new Bot(TOKEN);
 //api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/deleteWebhook?url=
-// app.use(webhookCallback(bot, "express"));
+app.use(webhookCallback(bot, "express"));
 
 app.get("/", (req, res) => {
   res.send("...Server is Running...");
 });
 
-// bot.command("start", async (ctx) => {
-//   await ctx.reply("Welcome to chat Bot Serega");
-// });
+bot.command("start", async (ctx) => {
+  await ctx.reply("Welcome to chat Bot Serega");
+});
 
 
 
