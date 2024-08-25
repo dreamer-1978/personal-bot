488371da23d560fafe15384d2743d86e479fc1a6
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 07:42:14 +0300

    add cors

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ import {
 } from "grammy";
 import express from 'express';
 import "dotenv/config";
+import cors from 'cors';
 
 const app = express()
+app.use(cors())
 const PORT = 3000
-
+const optionCors = {
+  origin: "*"
+}
 
 const TOKEN = process.env.BOT_TOKEN;
 
 // if (!TOKEN) throw new Error("Token not Found!");
-const bot = new Bot(TOKEN);
+// const bot = new Bot(TOKEN);
 //api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/deleteWebhook?url=
 // app.use(webhookCallback(bot, "express"));
 
-app.get("/", (req, res) => {
-  res.send("...Server is Running...");
+app.get("/", cors(optionCors), async (req, res) => {
+  await res.send("...Server is Running...");
 });
 
-bot.command("start", async (ctx) => {
-  await ctx.reply("Welcome to chat Bot Serega");
-});
+// bot.command("start", async (ctx) => {
+//   await ctx.reply("Welcome to chat Bot Serega");
+// });
 
 
 
