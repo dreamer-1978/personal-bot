05ae9cca58c00cffe914d2f11564e6e9bf97aca3
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 07:10:21 +0300

    vite bot

--- index.js
+++ index.js
@@ -13,25 +13,20 @@ const PORT = 3000
 const endpoint =
   "";
 const TOKEN = process.env.BOT_TOKEN;
+
 if (!TOKEN) throw new Error("Token not Found!");
 const bot = new Bot(TOKEN);
 //api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/deleteWebhook?url=
-https: app.use(webhookCallback(bot, "express"));
-
-  bot.command("start", async (ctx) => {
-    await ctx.reply("<b>Welcome to chat Bot Serega</b>", {
-      parse_mode: "HTML",
-    });
-  });
-
-    bot.hears(/about/, async (ctx) => {
-      const me = await bot.api.getMe();
-      ctx.reply(me.id);
-    });
+app.use(webhookCallback(bot, "express"));
 
 app.get("/", (req, res) => {
   res.send("...Server is Running...");
 });
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Welcome to chat Bot Serega");
+});
+
 
 
 
