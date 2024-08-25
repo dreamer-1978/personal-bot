b290871b839bb6503a98f3de0d035f88c8cbe9dc
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 22:58:26 +0300

    calback...

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import {
-  Bot,
-  InlineKeyboard,
-  webhookCallback,
-} from "grammy";
+import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 import "dotenv/config";
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-const PORT = 3000
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000;
 const optionCors = {
-  origin: "*"
-}
-
-
-
+  origin: "*",
+};
 
 const bot = new Bot(process.env.VITE_BOT_TOKEN);
 
-//api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/setWebhook?url=https://personal-bot-bice.vercel.app
-app.use(webhookCallback(bot, "express"));
+app.post(`/${bot.token}`, webhookCallback(bot, "express"));
 
 app.get("/", cors(optionCors), (req, res) => {
   res.send(`Server is Running! create Token:`);
@@ -28,11 +23,8 @@ app.get("/", cors(optionCors), (req, res) => {
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
-
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
 
-
+//api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/setWebhook?url=https://personal-bot-bice.vercel.app
