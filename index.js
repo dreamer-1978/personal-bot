289258a39c9d4b1e743dc8f73f95086a962a659a
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 08:26:52 +0300

    token created

--- index.js
+++ index.js
@@ -14,20 +14,18 @@ const optionCors = {
   origin: "*"
 }
 
-// const TOKEN = process.env.BOT_TOKEN;
+// const TOKEN = process.env.VITE_BOT_TOKEN;
 
 
-// const bot = new Bot(TOKEN);
+const bot = new Bot(process.env.VITE_BOT_TOKEN);
 //api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/deleteWebhook?url=
-// app.use(webhookCallback(bot, "express"));
+app.use(webhookCallback(bot, "express"));
 
-app.get("/", cors(optionCors), async (req, res) => {
-  await res.send(`Server is Running! create Token: ${process.env.BOT_TOKEN}`);
+app.get("/", cors(optionCors), (req, res) => {
+  res.send(`Server is Running! create Token:`);
 });
 
-// bot.command("start", async (ctx) => {
-//   await ctx.reply("Welcome to chat Bot Serega");
-// });
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 
 
