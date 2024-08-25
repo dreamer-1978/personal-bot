ee82ff4d3d75e653770f21d5dc23030ecf0ddff3
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 23:08:58 +0300

    calback...

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 const optionCors = {
