6b6b9f40e58acc81552d1698ca22d7ea3cc0db1e
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-25 07:19:46 +0300

    vite bot

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-import {
-  Bot,
-  GrammyError,
-  HttpError,
-  InlineKeyboard,
-  webhookCallback,
-} from "grammy";
+// import {
+//   Bot,
+//   GrammyError,
+//   HttpError,
+//   InlineKeyboard,
+//   webhookCallback,
+// } from "grammy";
 import express from 'express';
 import "dotenv/config";
 
 const app = express()
 const PORT = 3000
-const endpoint =
-  "";
-const TOKEN = process.env.BOT_TOKEN;
 
-if (!TOKEN) throw new Error("Token not Found!");
-const bot = new Bot(TOKEN);
+
+// const TOKEN = process.env.BOT_TOKEN;
+
+// if (!TOKEN) throw new Error("Token not Found!");
+// const bot = new Bot(TOKEN);
 //api.telegram.org/bot7344528094:AAFHZ5zofbZkdU9TX5fs4IWbpGIBPYBlcm0/deleteWebhook?url=
-app.use(webhookCallback(bot, "express"));
+// app.use(webhookCallback(bot, "express"));
 
 app.get("/", (req, res) => {
   res.send("...Server is Running...");
 });
 
-bot.command("start", async (ctx) => {
-  await ctx.reply("Welcome to chat Bot Serega");
-});
+// bot.command("start", async (ctx) => {
+//   await ctx.reply("Welcome to chat Bot Serega");
+// });
 
 
 
